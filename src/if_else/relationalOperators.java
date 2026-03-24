f63ba774805a1d5495df449d0b9226a943ba9d58
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-25 02:05:54 +0530

    new files add

---
+++ src/if_else/relationalOperators.java
@@ -0,0 +1,18 @@
+package if_else;
+
+public class relationalOperators {
+    public static void main(String[] args){
+        System.out.println(9<7);
+        System.out.println(9>7);
+        System.out.println(8==8);
+        System.out.println(9==7);
+        System.out.println(9==7+2);
+        System.out.println(6*7==42);
+        System.out.println(6*7!=42);
+
+        boolean x = true;// boolean me sirf true and false bus store kar sakte hai
+        boolean y = false;
+        boolean z= (5>3); // or kisis conditon ko store kar sakte hau numbers ko store nhi kar sakte hai
+        System.out.println(z);
+    }
+}
