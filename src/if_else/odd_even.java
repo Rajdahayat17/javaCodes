b515eedfba872c21a5a381521d124b5159e2af4d
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-26 23:26:24 +0530

    update

--- src/if_else/odd_even.java
+++ src/if_else/odd_even.java
@@ -1,7 +1,16 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class odd_even {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+  if(n%2==0){
+      System.out.println("even");
+  }
+  else{
+      System.out.println("odd");
+  }
     }
 }
