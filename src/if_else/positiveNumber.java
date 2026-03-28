9a8e9eb8050a439757b5f93d38fa879e8a952250
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-29 00:00:49 +0530

    new

--- src/if_else/positiveNumber.java
+++ src/if_else/positiveNumber.java
@@ -1,4 +1,16 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class positiveNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("new");
+        }
+    }
 }
