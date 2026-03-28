3a0a49158578c7eedfb0deab70c4dff70aed7408
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-29 00:35:29 +0530

    update

--- src/if_else/positiveNumber.java
+++ src/if_else/positiveNumber.java
@@ -7,7 +7,6 @@ public class positiveNumber {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number");
         int n = sc.nextInt();
-
         if(n >=0 ){
             System.out.println(n);
         }else {
--- src/if_else/realnumber.java
+++ src/if_else/realnumber.java
@@ -1,4 +1,19 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class realnumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        double n= sc.nextDouble();
+        int x= (int)n;
+       if(n-x>0){ // input = 4 and  x=4 answer = interger why 4-4=0 that is integer
+           // and intput is 6.5 and x=6 answer is  not interger why 6.5-6= 6 that is not interger
+           System.out.println("not an  integer");
+       }
+       else {
+           System.out.println("is a integer");
+       }
+    }
 }
