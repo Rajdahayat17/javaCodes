5652190202a3c5b707b6a73b6094db245fce201d
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-30 19:05:37 +0530

    update file

--- src/if_else/profitAndLoss.java
+++ src/if_else/profitAndLoss.java
@@ -11,9 +11,15 @@ public class profitAndLoss {
         System.out.println("enter the selling price");
         double sp = sc.nextDouble();
         if(sp>cp){
-            System.out.println("profit");
+            double profit = (sp-cp);
+            System.out.println("profit is "+ profit);
+            double profit_p = (profit/cp)*100;
+            System.out.println(profit_p);
         } else if (sp<cp) {
-            System.out.println("loss");
+            double loss = (cp-sp);
+            System.out.println("loss is "+  loss );
+            double loss_p = (loss/cp)*100;
+            System.out.println(loss_p);
         }
         else
         System.out.println("no profit no  loss");
