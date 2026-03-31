d0cf2c6a38e43863c1f504eee1cab93c3a0d49e8
Raj Dahayat <dahayatraj775@gmail.com>
2026-03-31 17:31:57 +0530

    update

--- src/if_else/printName.java
+++ src/if_else/printName.java
@@ -7,15 +7,20 @@ public class printName {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
         int nane = sc.nextInt();
-        if(nane % 5 == 0) {
-            System.out.println("Raj");
-        }
-        if(nane % 3 == 0){
-            System.out.println("Dahayat");
-        }
+
         if (nane % 5 == 0 && nane % 3 == 0) {
             System.out.println("Raj Dahayat");
         }
-        else System.out.println("Nothing");
+        else if (nane % 5 == 0) {
+            System.out.println("Raj");
+        }
+        else if (nane % 3 == 0) {
+            System.out.println("Dahayat");
+        }
+        else {
+            System.out.println("Nothing");
+        }
+
+        sc.close();
     }
 }
