10ef545dcdd51a2bf592dc1766269cb0e13be95c
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-01 23:17:50 +0530

    update

--- src/if_else/sideOftraraginl.java
+++ src/if_else/sideOftraraginl.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class sideOftraraginl {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("enter 15" +
+                "st side");
+        int a = input.nextInt();
+        System.out.println("enter 2nd side");
+        int b = input.nextInt();
+        System.out.println("enter 3rd side");
+        int c = input.nextInt();
+
+        if(a+b>c &&  b+c>a && c+a>b){
+            System.out.println("valid triangle");
+        }
+        else {
+            System.out.println("invalid triangle");
+        }
     }
 }
