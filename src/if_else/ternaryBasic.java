82d17e228becb671fd658a48497913d4206a754e
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-03 20:53:11 +0530

    update

--- src/if_else/ternaryBasic.java
+++ src/if_else/ternaryBasic.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class ternaryBasic {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+
+//        if (n % 2 == 0) {     // ye chiz hm jb use karet hai jab ham if ekse use karet hai ek or method hai is ko find karne ki
+//            System.out.println("even number");
+//        }else System.out.println("odd number");
+        System.out.println(n%2 ==0 ? "even" : "odd");
 
     }
 }
