0218ca31244fe41ea3df653a95ca4788381e3388
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-05 17:22:37 +0530

    Print N Number Of Name

--- src/Loops/BasicLoop.java
+++ src/Loops/BasicLoop.java
@@ -2,8 +2,11 @@ package Loops;
 
 public class BasicLoop {
     public static void main(String[] args){
-        for(int i=0;i<10;i++){
+        for(int i=7;i<=12;i++){
             System.out.println("hello raj ");
+        }
+        for(int j=0;j<=10;j++){
+            System.out.println(j);
         }
     }
 }
