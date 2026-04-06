7db86e99050707a4d9e86233ec9668c2e43ec809
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-06 23:59:17 +0530

    code updated

--- src/Loops/printNumberfromOnetoN.java
+++ src/Loops/printNumberfromOnetoN.java
@@ -1,4 +1,14 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class printNumberfromOnetoN {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n= sc.nextInt();
+        for (int i=n;i>=1;i--){
+            System.out.println(i);
+        }
+    }
 }
