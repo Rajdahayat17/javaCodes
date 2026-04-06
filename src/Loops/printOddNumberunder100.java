9929e721d9d70f5e7807cc8dd3d962b13c0872a6
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-06 23:47:52 +0530

    add code

--- src/Loops/printOddNumberunder100.java
+++ src/Loops/printOddNumberunder100.java
@@ -1,4 +1,13 @@
 package Loops;
 
 public class printOddNumberunder100 {
+    public static void main(String[] args){
+        for( int i=1; i<=100;i++){  // ye ho gai fast method even number print karne ke
+            if(i%3 == 0) System.out.print(i+" ");
+        }
+        System.out.println( );
+        for(int j=1; j<+100;j++){
+            if(j%2 != 0) System.out.print(j+" ");
+        }
+    }
 }
