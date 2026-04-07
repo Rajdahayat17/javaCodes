b480a6d1c3719fba1b71f7dd52dcbcb5e004b189
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-07 23:56:11 +0530

    updated

--- src/Loops/DisplayAP.java
+++ src/Loops/DisplayAP.java
@@ -1,4 +1,14 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class DisplayAP {
+      public static void main(String[] args){
+          Scanner sc= new Scanner(System.in);
+          System.out.println("enter n number");
+          int n = sc.nextInt();
+          for(int i=2;i<=3*n-1;i+=3){
+              System.out.print(i+" ");
+      }
+}
 }
