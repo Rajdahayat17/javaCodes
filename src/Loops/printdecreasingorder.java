7276f4ab502414cd57943ef0553047383772ab4f
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-09 23:57:39 +0530

    updated

---
+++ src/Loops/printdecreasingorder.java
@@ -0,0 +1,25 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class printdecreasingorder {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter n number");
+        int n = sc.nextInt();
+        //2,25,8,11......    // 1st method
+        for(int i = 3*n - 1; i >= 1; i -= 3){
+            System.out.print(i+" ");
+        }
+//        System.out.println(" ");
+//        // 2nd method
+//        // without using AP formula
+//        System.out.println("enter the n");
+//        int n1 = sc.nextInt();
+//        int a=2,d=3;
+//        for (int i= 1;i<=n1;i++){
+//            System.out.print(a+" ");
+//            a+=d;
+        }
+    }
+
