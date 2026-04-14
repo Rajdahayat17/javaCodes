f605eec111f8c45dfb58d9e88532b1373a0b696d
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-14 23:53:09 +0530

    update

--- src/Loops/WAPcheckGivenNoIsPrimeOrNot.java
+++ src/Loops/WAPcheckGivenNoIsPrimeOrNot.java
@@ -1,4 +1,26 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class WAPcheckGivenNoIsPrimeOrNot {
+    public static  void main(String[] args){
+       // Scanner sc= new Scanner(System.in);
+        //int n= sc.nextInt();
+       // System.out.println("Enter the number : ");
+        int n = 55;
+        boolean isPrime = true;
+
+        if(n <= 1) isPrime = false;
+
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime) System.out.println("Prime");
+        else System.out.println("Not Prime");
+
+    }
 }
