b8142e5285882c0463482b64eb12a80182bcc905
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-15 01:04:23 +0530

    updated

--- src/Loops/continues.java
+++ src/Loops/continues.java
@@ -1,4 +1,11 @@
 package Loops;
 
 public class continues {
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+            System.out.println(i);
+            if(i==13)continue; // continue ka mtlb hota hai kisi ko skip karna jaise isme 13 skip ho gya
+            System.out.println("welcome");
+        }
+    }
 }
