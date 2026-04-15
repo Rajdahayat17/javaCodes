361bdedf8e379b48e696aa3704de98f9bc4ee9cb
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-15 23:50:01 +0530

    updated

--- src/Loops/whileLoop.java
+++ src/Loops/whileLoop.java
@@ -1,4 +1,11 @@
 package Loops;
 
 public class whileLoop {
+    public static void main(String[] args) {
+        int n=1;
+        while (n<=10){
+            System.out.println(n);
+            n++;
+        }
+    }
 }
