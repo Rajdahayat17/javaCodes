245b9198f307be4cfc91322cd1dd41105756b281
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-16 02:29:20 +0530

    update new files

---
+++ src/Loops/LimitsOfint.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class LimitsOfint {
+    public static void main(String[] args) {
+        int x= Integer.MAX_VALUE;
+        int y=Integer.MIN_VALUE;
+
+        char u= Character.MAX_VALUE;
+        char w= Character.MIN_VALUE;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println((int)u);
+        System.out.println((int)w);
+
+    }
+}
---
+++ src/Loops/printSumofDigits.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class printSumofDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        int sum=0;
+        while(n!=0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println(sum);
+    }
+}
