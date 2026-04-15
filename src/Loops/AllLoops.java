280e305abeb66bc9dcb6db9d6c29a92f2b226fcb
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-16 00:24:15 +0530

    new all loops file add

---
+++ src/Loops/AllLoops.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class AllLoops {
+    public static void main(String[] args) {
+        int i=1;
+        for(i=1;i<=10;i++){
+            System.out.print(i+ " ");
+        }
+        System.out.println();
+        int j=1;
+        while (j<=10){
+            System.out.print(j+ " ");
+            j++;
+        }
+        System.out.println();
+        int z=1;
+        do {
+            System.out.print(z+ " ");
+            z++;
+        } while (z <= 10);
+    }
+}
