56439b92b64079421726e2d0d8de055f974ee75e
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-17 13:11:33 +0530

    updated

--- src/Pattern_Printing/StarRectangle.java
+++ src/Pattern_Printing/StarRectangle.java
@@ -1,4 +1,17 @@
 package Pattern_Printing;
 
+import java.util.Scanner;
+
 public class StarRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {   // fixed
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");   // fixed
+            }
+            System.out.println();         // moved inside
+        }
+    }
 }
