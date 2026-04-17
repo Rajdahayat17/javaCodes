2ab02cc0c236ba8490b3003f1039e7bc287876b6
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-17 23:56:49 +0530

    new updated

--- src/Pattern_Printing/StarRectangleABC.java
+++ src/Pattern_Printing/StarRectangleABC.java
@@ -10,14 +10,31 @@ public class StarRectangleABC {
 
         for (int i = 1; i <= row; i++) { // kitni line hogi
             for (int j = 1; j <= col; j++) { // har line me kitne honge
-                System.out.print((char)(i+64)+" "); // print 11111
+                System.out.print((char) (i + 64) + " "); // print AAAA
             }
             System.out.println();         // moved inside
         }
         System.out.println();
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= col; j++) {
-                System.out.print((char) (j+96) +" "); // print 12345
+                System.out.print((char) (j + 64) + " "); // print AB
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print((char) (i + 96) + " "); // print aa
+            }
+            System.out.println();         // moved inside
+        }
+        System.out.println();
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print((char) (j + 96) + " "); // print ab
             }
             System.out.println();
         }
