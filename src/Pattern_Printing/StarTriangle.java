4c3729adcd311855bb2251bd12063d51a0bba845
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-18 01:48:07 +0530

    code improvement (no feature change)

--- src/Pattern_Printing/StarTriangle.java
+++ src/Pattern_Printing/StarTriangle.java
@@ -5,10 +5,73 @@ import java.util.Scanner;
 public class StarTriangle {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
+
+        int n = sc.nextInt();
+
+
+        // ⭐ Star Triangle
         for (int i=1;i<=n;i++){
             for (int j=1; j<=i; j++){
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 111 triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i +" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // 123 triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // AAA triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (i + 64) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // ABC triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (j + 64) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // aaa triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (i + 96) + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // abc triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (j + 96) + " ");
             }
             System.out.println();
         }
