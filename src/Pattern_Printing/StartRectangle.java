e76f65aa846d59ee40f6779f62a812896c50d747
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-17 23:42:19 +0530

    new file add update the files

--- src/Pattern_Printing/StartRectangle.java
+++ src/Pattern_Printing/StartRectangle.java
@@ -2,7 +2,7 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class StarRectangle {
+public class StartRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
@@ -10,9 +10,16 @@ public class StarRectangle {
 
         for (int i = 1; i <= row; i++) { // kitni line hogi
             for (int j = 1; j <= col; j++) { // har line me kitne honge
-                System.out.print("* ");
+                System.out.print(i +" "); // print 11111
             }
             System.out.println();         // moved inside
+        }
+        System.out.println();
+        for (int i = 1; i <= row; i++) { // kitni line hogi
+            for (int j = 1; j <= col; j++) { // har line me kitne honge
+                System.out.print(j +" "); // print 12345
+            }
+            System.out.println();
         }
     }
 }
