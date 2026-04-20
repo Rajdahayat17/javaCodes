5fba4842cdcbadeedd7ce518372208d94513791a
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-21 01:07:39 +0530

    code improve

--- src/Pattern_Printing/HollowRectangle.java
+++ src/Pattern_Printing/HollowRectangle.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class HollowRectangle {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-
-        int n = sc.nextInt();
+        int row = sc.nextInt();
+        int col = sc.nextInt();
 
 
         // ⭐ Star Triangle
-        for (int i=1;i<=n;i++){
-            for (int j=1; j<=n; j++){
-                System.out.print("* ");
+        for (int i=1;i<=row;i++){
+            for (int j=1; j<=col; j++){
+                if (i==1 || i==row || j==1 || j==col) {
+                    System.out.print( "* ");              // odd row → numbers
+                } else {
+                    System.out.print("  "); // even row → A B C
+                }
             }
             System.out.println();
-        }
-        System.out.println();
     }
+}
 }
