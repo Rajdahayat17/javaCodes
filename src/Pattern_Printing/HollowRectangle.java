65ef827b3d6ed055cf7634a65dbbb60403abdfe6
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-20 23:59:54 +0530

    updated

--- src/Pattern_Printing/HollowRectangle.java
+++ src/Pattern_Printing/HollowRectangle.java
@@ -1,7 +1,21 @@
 package Pattern_Printing;
 
+import java.util.Scanner;
+
 public class HollowRectangle {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+
+        // ⭐ Star Triangle
+        for (int i=1;i<=n;i++){
+            for (int j=1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
