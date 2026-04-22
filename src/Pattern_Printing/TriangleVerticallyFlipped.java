2d73a8e750361cfd80922f04e2f54749ae7b7605
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-23 01:16:57 +0530

    code inprove

--- src/Pattern_Printing/TriangleVerticallyFlipped.java
+++ src/Pattern_Printing/TriangleVerticallyFlipped.java
@@ -7,12 +7,118 @@ public class TriangleVerticallyFlipped {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-
-        for (int i = 1; i <= n; i++) { // kitni line hogi
-            for (int j = 1; j <= n; j++) { // har line me kitne honge
-                System.out.print("* ");
+        // ⭐ Star Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
-            System.out.println();         // moved inside
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // 111 Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // 123 Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print(j + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // AAA Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print((char) (i + 64) + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // ABC Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print((char) (j + 64) + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // aaa Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print((char) (i + 96) + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // abc Pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print((char) (j + 96) + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if ((i + j) > n) {
+                    if (i % 2 != 0) {
+                        System.out.print(j + " ");              // odd row → numbers
+                    } else {
+                        System.out.print((char) (j + 64) + " "); // even row → A B C
+                    }
+                } else {
+                    System.out.print("  "); // spacing fix
+                }
+
+            }
+            System.out.println(); // line break important
+
         }
     }
 }
