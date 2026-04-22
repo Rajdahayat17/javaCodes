e1ea31ef2893c63300d34719436903cd0f3c3250
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-22 23:53:16 +0530

    updated

--- src/Pattern_Printing/TriangleVerticallyFlipped.java
+++ src/Pattern_Printing/TriangleVerticallyFlipped.java
@@ -1,4 +1,18 @@
 package Pattern_Printing;
 
+import java.util.Scanner;
+
 public class TriangleVerticallyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for (int i = 1; i <= n; i++) { // kitni line hogi
+            for (int j = 1; j <= n; j++) { // har line me kitne honge
+                System.out.print("* ");
+            }
+            System.out.println();         // moved inside
+        }
+    }
 }
