6b642af171b8a54f1672c05394203706fc98396e
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-27 23:29:03 +0530

    new file add

---
+++ src/Pattern_Printing/BridgePrint.java
@@ -0,0 +1,10 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class BridgePrint {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
