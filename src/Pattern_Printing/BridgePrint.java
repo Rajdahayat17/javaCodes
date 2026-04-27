384ddfac920dcb3b07def593acf2a3770d901d99
Raj Dahayat <dahayatraj775@gmail.com>
2026-04-27 23:45:54 +0530

    improve a code

--- src/Pattern_Printing/BridgePrint.java
+++ src/Pattern_Printing/BridgePrint.java
@@ -3,8 +3,33 @@ package Pattern_Printing;
 import java.util.Scanner;
 
 public class BridgePrint {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int nsp = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Left stars
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("* ");
+            }
+
+            // Middle spaces
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("* ");
+            }
+
+            nsp += 2;
+
+            System.out.println();
+        }
     }
 }
