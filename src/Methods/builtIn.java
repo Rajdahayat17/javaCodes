f94222ab75c8435f4244d29c0b38bc7d02369427
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-01 23:52:39 +0530

    improve

--- src/Methods/builtIn.java
+++ src/Methods/builtIn.java
@@ -2,6 +2,8 @@ package Methods;
 
 public class builtIn {
     public static void main(String[] args){
-
+        // Math. wale fuction first me padhna hai
+        //double x= Math
+        System.out.println(Math.sqrt(9.8));
     }
 }
