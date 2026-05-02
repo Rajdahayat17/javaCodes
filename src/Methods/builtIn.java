b92324ac9e3d66fa4cf0e2c0fbe3cfde2e7da9c7
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-02 23:49:43 +0530

    improve

--- src/Methods/builtIn.java
+++ src/Methods/builtIn.java
@@ -3,7 +3,7 @@ package Methods;
 public class builtIn {
     public static void main(String[] args){
         // Math. wale fuction first me padhna hai
-        //double x= Math
+        // Sabhi Prakar ke function hame mil jate hai  jisse ham Formul se use karte hai
         System.out.println(Math.sqrt(9.8));
         System.out.println(Math.cbrt(100));
 
