78f9f85b6dfeb59a499d084c1ebdb72bc02ffe27
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-02 23:53:24 +0530

    improve

--- src/Methods/builtIn.java
+++ src/Methods/builtIn.java
@@ -6,6 +6,10 @@ public class builtIn {
         // Sabhi Prakar ke function hame mil jate hai  jisse ham Formul se use karte hai
         System.out.println(Math.sqrt(9.8));
         System.out.println(Math.cbrt(100));
+        System.out.println(Math.floor(-7.8));
+        System.out.println(Math.ceil(3.002));
+        System.out.println(Math.max(2.3,2.5));
+        System.out.println(Math.min(2.3,2.5));
 
     }
 }
