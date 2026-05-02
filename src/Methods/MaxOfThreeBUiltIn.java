91cc7bad41a23e90b4e52dbabecae5f49fd9c4f5
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-02 23:59:08 +0530

    improve

--- src/Methods/MaxOfThreeBUiltIn.java
+++ src/Methods/MaxOfThreeBUiltIn.java
@@ -1,4 +1,14 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class MaxOfThreeBUiltIn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+
+    }
 }
