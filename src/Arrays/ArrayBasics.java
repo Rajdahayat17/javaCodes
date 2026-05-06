c4e5149cf1b9041d2b9ed1266265883d181b0dfd
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-06 23:55:39 +0530

    improve

--- src/Arrays/ArrayBasics.java
+++ src/Arrays/ArrayBasics.java
@@ -1,4 +1,9 @@
 package Arrays;
 
 public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] x = {6, 5, 4, 5, 4, 5, 4};
+        // indexing
+        System.out.println(x[5]);
+    }
 }
