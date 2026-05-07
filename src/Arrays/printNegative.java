3a4c00ab7518c030e04c0a66031195407c1c73ed
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-07 23:59:04 +0530

    add file and code

--- src/Arrays/printNegative.java
+++ src/Arrays/printNegative.java
@@ -1,4 +1,12 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class printNegative {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Array Size");
+        int n= sc.nextInt();
+
+    }
 }
