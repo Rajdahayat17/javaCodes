bee55df84c4f07ead1f8a5d3a3a3aef7a4309a9e
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-12 02:11:43 +0530

    improve code

--- src/Arrays/PassingArrayToMethod.java
+++ src/Arrays/PassingArrayToMethod.java
@@ -1,5 +1,23 @@
 package Arrays;
 
-public class PassingArrayToMethod
-{
+public class PassingArrayToMethod {
+
+    public static void main(String[] args) {
+
+        // Array declaration
+        int x[] = {10, 3, 29, 39};
+        System.out.println(x[2]);
+        // Method call
+        change(x);
+
+        // Print updated value
+        System.out.println(x[2]);
+    }
+
+    // Method to modify array
+    public static void change(int[] x) {
+
+        // Change value at index 2
+        x[2] = 99;
+    }
 }
