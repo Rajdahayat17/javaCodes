bfd67d746bcfe22bacfed63eac073c0def05eba3
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-11 23:43:11 +0530

    improve

--- src/Arrays/ArrayOtherDataType.java
+++ src/Arrays/ArrayOtherDataType.java
@@ -1,4 +1,10 @@
 package Arrays;
 
 public class ArrayOtherDataType {
+    public static void main(String[] args) {
+        double [] arr= new double[5];
+        for (int i=0;i< arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
 }
