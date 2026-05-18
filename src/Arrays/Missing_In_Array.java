778ba22f6f75b10b2d6490ba8287fdf91e54c900
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-18 23:44:58 +0530

    improve code

--- src/Arrays/Missing_In_Array.java
+++ src/Arrays/Missing_In_Array.java
@@ -1,4 +1,47 @@
 package Arrays;
 
 public class Missing_In_Array {
+
+    public static void main(String[] args) {
+
+        // Array with one missing number
+        int[] arr = {1, 2, 4, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+            // Create object of class
+            Missing_In_Array obj = new Missing_In_Array();
+
+            // Call method and store answer
+            int ans = obj.missingNum(arr);
+
+            // Print missing number
+            System.out.println("Missing Number is: " + ans);
+        }
+
+    // Method to find missing number
+    int missingNum(int arr[]) {
+
+        // Actual count of numbers including missing number
+        long n = arr.length + 1;
+
+        // Sum of numbers from 1 to n
+        // Formula: n(n + 1) / 2
+        long sum = n * (n + 1) / 2;
+
+        // Variable to store array sum
+        long arraySum = 0;
+
+        // Traverse array and calculate sum
+        for (int ele : arr) {
+
+            // Add current element into arraySum
+            arraySum += ele;
+        }
+
+        // Missing number = Expected sum - Actual array sum
+        return (int) (sum - arraySum);
+    }
 }
