4ab7391e9e240edd6c0820af3b8b243a839bf34c
Raj Dahayat <dahayatraj775@gmail.com>
2026-05-23 23:43:07 +0530

    Rename file

--- src/Arrays/Array_Basics.java
+++ src/Arrays/Array_Basics.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class ArrayBasics {
+public class Array_Basics {
     public static void main(String[] args) {
         int[] x = {6, 5, 4, 5, 4, 5, 4};
         // indexing
