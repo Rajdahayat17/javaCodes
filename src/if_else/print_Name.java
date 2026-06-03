aa7c82146e9f5cb691ba92a37ee3a4daaf82888f
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-04 00:05:50 +0530

    file update

--- src/if_else/print_Name.java
+++ src/if_else/print_Name.java
@@ -2,7 +2,7 @@ package if_else;
 
 import java.util.Scanner;
 
-public class printName {
+public class print_Name {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
