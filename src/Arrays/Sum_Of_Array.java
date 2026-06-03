7e5c29a95b8416e5a4b3d069eec8c91133439600
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-03 23:57:17 +0530

    Rename file

--- src/Arrays/Sum_Of_Array.java
+++ src/Arrays/Sum_Of_Array.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class SumOfArray {
+public class Sum_Of_Array {
     public static void main(String[] args) {
         int arr[]={5,6,8,6};
         int sum=0; // ise 0 se define karna hai
