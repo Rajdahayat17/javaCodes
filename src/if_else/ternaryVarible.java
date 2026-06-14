3c42ef547d823b05e3444b0554f00c11b52aab83
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-14 17:57:34 +0530

    file update

--- src/if_else/ternaryVarible.java
+++ src/if_else/ternaryVarible.java
@@ -3,12 +3,26 @@ package if_else;
 import java.util.Scanner;
 
 public class ternaryVarible {
-     public static void main(String[] args){
-         Scanner sc = new Scanner(System.in);
-         System.out.println("enter the number :");
-         int n = sc.nextInt();
+    public static void main(String[] args) {
 
-         int abhi = (n>=0)? 100 :0;
-         System.out.println("abhi");
-     }
+        // User se input lene ke liye Scanner object banaya
+        Scanner sc = new Scanner(System.in);
+
+        // User se number input karne ko bola
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+
+        /*
+         * Ternary Operator:
+         * Syntax:
+         * condition ? value_if_true : value_if_false;
+         *
+         * Agar n >= 0 hai to abhi me 100 store hoga,
+         * warna abhi me 0 store hoga.
+         */
+        int abhi = (n >= 0) ? 100 : 0;
+
+        // Variable ki value print karo
+        System.out.println(abhi);
+    }
 }
