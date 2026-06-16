684ab857890413d134b869aeff5cb9ee0e8d3cbc
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-16 23:31:19 +0530

    file update

--- src/Methods/PermutationAndCombination.java
+++ src/Methods/PermutationAndCombination.java
@@ -3,19 +3,51 @@ package Methods;
 import java.util.Scanner;
 
 public class PermutationAndCombination {
-    public static int fact(int x){
-        int f=1;
-        for(int i=1;i<=x;i++){
-            f *=i;
+
+    // Method to calculate factorial of a number
+    public static long fact(int x) {
+        long f = 1;
+
+        // Multiply all numbers from 1 to x
+        // Example: 5! = 1 × 2 × 3 × 4 × 5 = 120
+        for (int i = 1; i <= x; i++) {
+            f *= i;
         }
+
         return f;
     }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read values of n and r
+        System.out.print("Enter value of n: ");
         int n = sc.nextInt();
+
+        System.out.print("Enter value of r: ");
         int r = sc.nextInt();
-        int ncr = fact(n)/fact(r)*fact(n-r);
-        int npr = fact(n)/fact(n-r);
-        System.out.println(ncr+" "+npr);
+
+        // Check if input is valid
+        // r cannot be greater than n
+        // n and r cannot be negative
+        if (n < 0 || r < 0 || r > n) {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        // Combination Formula:
+        // nCr = n! / (r! * (n-r)!)
+        long ncr = fact(n) / (fact(r) * fact(n - r));
+
+        // Permutation Formula:
+        // nPr = n! / (n-r)!
+        long npr = fact(n) / fact(n - r);
+
+        // Display the results
+        System.out.println("Combination (nCr) = " + ncr);
+        System.out.println("Permutation (nPr) = " + npr);
+
+        sc.close();
     }
 }
