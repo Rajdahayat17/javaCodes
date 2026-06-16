70e1b1e8b2afb6730c71ed5ca13de62f6955a69e
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-16 23:33:40 +0530

    improve code

--- src/Methods/Swap.java
+++ src/Methods/Swap.java
@@ -4,13 +4,26 @@ import java.util.Scanner;
 
 public class Swap {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read two numbers from the user
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a+ " " +b);
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println(a+ " " +b);
+
+        // Print original values before swapping
+        System.out.println(a + " " + b);
+
+        // Store the value of a in a temporary variable
+        int temp = a;
+
+        // Assign the value of b to a
+        a = b;
+
+        // Assign the stored value of a (temp) to b
+        b = temp;
+
+        // Print values after swapping
+        System.out.println(a + " " + b);
     }
 }
