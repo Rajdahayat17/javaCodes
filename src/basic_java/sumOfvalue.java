bc3b36c307939a051adc5c64e947174411e480b2
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-17 23:33:47 +0530

    file update

--- src/basic_java/sumOfvalue.java
+++ src/basic_java/sumOfvalue.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 
 public class sumOfvalue {
     public static void main(String[] args) {
-        int a,b,c;
+
+        // Declare three integer variables
+        int a, b, c;
+
+        // Create Scanner object to take input from the user
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter 1st");
-        a= sc.nextInt();
-        System.out.println("enter 2nd");
-        b= sc.nextInt();
-        System.out.println("enter 3rd");
-        c= sc.nextInt();
 
-        System.out.println(a+b+c);
+        // Take first number as input
+        System.out.println("Enter 1st number");
+        a = sc.nextInt();
 
+        // Take second number as input
+        System.out.println("Enter 2nd number");
+        b = sc.nextInt();
+
+        // Take third number as input
+        System.out.println("Enter 3rd number");
+        c = sc.nextInt();
+
+        // Add all three numbers and print the result
+        System.out.println(a + b + c);
+
+        sc.close();
     }
 }
