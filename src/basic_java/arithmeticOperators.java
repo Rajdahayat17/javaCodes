51d942d3a23b265ab03e5864355d64d09b92df6f
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-18 23:02:27 +0530

    improve code

--- src/basic_java/arithmeticOperators.java
+++ src/basic_java/arithmeticOperators.java
@@ -2,20 +2,39 @@ package basic_java;
 
 public class arithmeticOperators {
     public static void main(String[] args){
-        int a = 7 ,b = 4;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
 
-        double x = 34 ,y= 16;
-        System.out.println(x+y);
-        System.out.println(x-y);
-        System.out.println(x*y);
-        System.out.println(x/y);
+        // Declare and initialize two integer variables
+        int a = 7, b = 4;
+
+        // Addition of two integers
+        System.out.println(a + b);
+
+        // Subtraction of two integers
+        System.out.println(a - b);
+
+        // Multiplication of two integers
+        System.out.println(a * b);
+
+        // Integer division
+        // Since both operands are integers, the result is also an integer (fractional part is discarded)
+        System.out.println(a / b);
 
 
 
+        // Declare and initialize two double variables
+        double x = 34, y = 16;
+
+        // Addition of two double values
+        System.out.println(x + y);
+
+        // Subtraction of two double values
+        System.out.println(x - y);
+
+        // Multiplication of two double values
+        System.out.println(x * y);
+
+        // Floating-point division
+        // Since operands are doubles, the result includes decimal precision
+        System.out.println(x / y);
     }
-
 }
