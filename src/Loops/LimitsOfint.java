3f3a21a7a131564208d98ec030f7a462b7325a69
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-19 12:56:20 +0530

    file update

--- src/Loops/LimitsOfint.java
+++ src/Loops/LimitsOfint.java
@@ -2,22 +2,40 @@ package Loops;
 
 public class LimitsOfint {
     public static void main(String[] args) {
-        int x= Integer.MAX_VALUE;
-        int y=Integer.MIN_VALUE;
-        long a=Long.MAX_VALUE;
-        long b=Long.MIN_VALUE;
 
-        char u= Character.MAX_VALUE;
-        char w= Character.MIN_VALUE;
+        // Integer limits
+        // MAX_VALUE stores the largest value that can be held by an int (32 bits)
+        int x = Integer.MAX_VALUE;
 
+        // MIN_VALUE stores the smallest value that can be held by an int
+        int y = Integer.MIN_VALUE;
+
+        // Long limits
+        // MAX_VALUE stores the largest value that can be held by a long (64 bits)
+        long a = Long.MAX_VALUE;
+
+        // MIN_VALUE stores the smallest value that can be held by a long
+        long b = Long.MIN_VALUE;
+
+        // Character limits
+        // char is an unsigned 16-bit data type in Java
+        // MAX_VALUE represents the largest Unicode value (65535)
+        char u = Character.MAX_VALUE;
+
+        // MIN_VALUE represents the smallest Unicode value (0)
+        char w = Character.MIN_VALUE;
+
+        // Display int limits
         System.out.println(x);
         System.out.println(y);
 
+        // Display long limits
         System.out.println(a);
         System.out.println(b);
 
-        System.out.println((int)u);
-        System.out.println((int)w);
-
+        // char values are displayed as integers using type casting
+        // because printing them directly may show Unicode characters
+        System.out.println((int) u); // 65535
+        System.out.println((int) w); // 0
     }
 }
