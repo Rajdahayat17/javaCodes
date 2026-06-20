8ba93950395ec23ca299c41e7433a4f49f311400
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-20 23:49:08 +0530

    file update

--- src/basic_java/numberprint.java
+++ src/basic_java/numberprint.java
@@ -3,10 +3,14 @@ package basic_java;
 public class numberprint {
     public static void main(String[] args) {
 
+        // Prints the integer value 87
         System.out.println(87);
+
+        // Prints the text "x+10" exactly as it is because it is inside double quotes
         System.out.println("x+10");
-        System.out.println(10+87);
+
+        // Adds 10 and 87, then prints the result (97)
+        System.out.println(10 + 87);
 
     }
 }
-
