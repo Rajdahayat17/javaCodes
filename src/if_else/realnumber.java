8f56c82cae5805aa66159a4e8a528c614b249651
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-24 22:40:37 +0530

    file update

--- src/if_else/realnumber.java
+++ src/if_else/realnumber.java
@@ -3,17 +3,33 @@ package if_else;
 import java.util.Scanner;
 
 public class realnumber {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter number");
-        double n= sc.nextDouble();
-        int x= (int)n;
-       if(n-x>0){ // input = 4 and  x=4 answer = interger why 4-4=0 that is integer
-           // and intput is 6.5 and x=6 answer is  not interger why 6.5-6= 6 that is not interger
-           System.out.println("not an  integer");
-       }
-       else {
-           System.out.println("is a integer");
-       }
+
+        System.out.println("Enter a number");
+        double n = sc.nextDouble();
+
+        // Type casting converts the decimal number into an integer
+        // Any fractional part is removed
+        // Example:
+        // n = 6.5  -> x = 6
+        // n = 4.0  -> x = 4
+        int x = (int) n;
+
+        // If the difference between the original number and
+        // the integer part is greater than 0, then the number
+        // contains a decimal part and is NOT an integer.
+        //
+        // Examples:
+        // n = 6.5, x = 6
+        // n - x = 0.5 > 0  -> Not an Integer
+        //
+        // n = 4.0, x = 4
+        // n - x = 0 -> Integer
+        if (n - x > 0) {
+            System.out.println("Not an Integer");
+        } else {
+            System.out.println("Is an Integer");
+        }
     }
 }
