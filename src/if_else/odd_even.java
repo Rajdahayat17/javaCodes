12e7bb5af8e31a36359aee756d94b867431a46e3
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-24 22:41:56 +0530

    improve code

--- src/if_else/odd_even.java
+++ src/if_else/odd_even.java
@@ -3,14 +3,33 @@ package if_else;
 import java.util.Scanner;
 
 public class odd_even {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read a number from the user
         int n = sc.nextInt();
-  if(n%2==0){
-      System.out.println("even");
-  }
-  else{
-      System.out.println("odd");
-  }
+
+        // The modulus operator (%) gives the remainder
+        // after division.
+        //
+        // If a number is divisible by 2, the remainder is 0,
+        // so the number is even.
+        //
+        // Examples:
+        // 8 % 2 = 0  -> Even
+        // 10 % 2 = 0 -> Even
+        if (n % 2 == 0) {
+            System.out.println("even");
+        }
+
+        // If the remainder is not 0, the number is odd.
+        //
+        // Examples:
+        // 7 % 2 = 1 -> Odd
+        // 15 % 2 = 1 -> Odd
+        else {
+            System.out.println("odd");
+        }
     }
 }
