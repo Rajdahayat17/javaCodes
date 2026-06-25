1fd8ac408e158ab040b8628b32b16d15ab1cf4e0
Raj Dahayat <dahayatraj775@gmail.com>
2026-06-25 21:52:58 +0530

    file update

--- src/if_else/get3inputPrintGreaterNumber.java
+++ src/if_else/get3inputPrintGreaterNumber.java
@@ -4,23 +4,36 @@ import java.util.Scanner;
 
 public class get3inputPrintGreaterNumber {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter 1st number");
+        // Take first number as input
+        System.out.println("Enter 1st number");
         int n = sc.nextInt();
 
-        System.out.println("enter 2nd number");
+        // Take second number as input
+        System.out.println("Enter 2nd number");
         int m = sc.nextInt();
 
-        System.out.println("enter 3rd number");
+        // Take third number as input
+        System.out.println("Enter 3rd number");
         int d = sc.nextInt();
 
+        // Check if n is greater than both m and d
+        // If true, n is the greatest number
         if (n > m && n > d) {
             System.out.println("Greatest number is: " + n);
         }
+
+        // If n is not the greatest, check whether
+        // m is greater than both n and d
+        // If true, m is the greatest number
         else if (m > n && m > d) {
             System.out.println("Greatest number is: " + m);
         }
+
+        // If neither n nor m is the greatest,
+        // then d must be the greatest number
         else {
             System.out.println("Greatest number is: " + d);
         }
