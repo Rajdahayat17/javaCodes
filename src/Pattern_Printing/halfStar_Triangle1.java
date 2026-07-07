06f0e72792ae51d77742d399ce045840bccaba4c
Raj Dahayat <dahayatraj775@gmail.com>
2026-07-07 23:11:58 +0530

    update

--- src/Pattern_Printing/halfStar_Triangle1.java
+++ src/Pattern_Printing/halfStar_Triangle1.java
@@ -2,7 +2,7 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class halfStarTriangle1 {
+public class halfStar_Triangle1 {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
 
