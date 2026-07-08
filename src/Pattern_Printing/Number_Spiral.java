f1da156b76e86a30c76a2617c61c1de093b1c52c
Raj Dahayat <dahayatraj775@gmail.com>
2026-07-08 23:34:40 +0530

    improve

--- src/Pattern_Printing/Number_Spiral.java
+++ src/Pattern_Printing/Number_Spiral.java
@@ -2,7 +2,7 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class NumberSpiral {
+public class Number_Spiral {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
