5534f26fa9e194b1a53d2bb7a9fc4f6cb4fe2471
Raj Dahayat <dahayatraj775@gmail.com>
2026-07-13 23:28:21 +0530

    update

--- src/Pattern_Printing/Star_Cross.java
+++ src/Pattern_Printing/Star_Cross.java
@@ -2,7 +2,7 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class StarCross {
+public class Star_Cross {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
