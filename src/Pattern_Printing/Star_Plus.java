7ffca3def7fbbf309f5410d108c3e54e560ef121
Raj Dahayat <dahayatraj775@gmail.com>
2026-07-14 23:29:21 +0530

    update

--- src/Pattern_Printing/Star_Plus.java
+++ src/Pattern_Printing/Star_Plus.java
@@ -2,7 +2,7 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
-public class StarPlus {
+public class Star_Plus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
