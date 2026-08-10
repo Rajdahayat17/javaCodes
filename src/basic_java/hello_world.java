ca0086588a5581bb8e049304f3f1167ad7585d79
Raj Dahayat <dahayatraj775@gmail.com>
2026-08-10 23:16:31 +0530

    update

--- src/basic_java/hello_world.java
+++ src/basic_java/hello_world.java
@@ -1,6 +1,6 @@
 package basic_java;
 
-public class helloworld {
+public class hello_world {
    public static void main(String[] args) {
        System.out.print("hello worrld \n");
        System.out.println("hello raj");
