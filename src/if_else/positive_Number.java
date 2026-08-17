ad48ee178648a80bf5fbe9d16acffe4d15d4ae8a
Raj Dahayat <dahayatraj775@gmail.com>
2026-08-17 23:42:41 +0530

    update

--- src/if_else/positive_Number.java
+++ src/if_else/positive_Number.java
@@ -2,7 +2,7 @@ package if_else;
 
 import java.util.Scanner;
 
-public class positiveNumber {
+public class positive_Number {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number");
